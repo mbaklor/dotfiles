d41ae31d335894f723c62a581566a0fc78607837
mbaklor <mbaklor@gmail.com>
2025-02-19 20:15:10 +0200

    add(yazi): linemode shows size and date modified

---
+++ yazi/init.lua
@@ -0,0 +1,12 @@
+function Linemode:size_and_mtime()
+    local time = math.floor(self._file.cha.mtime or 0)
+    if time == 0 then
+        time = ""
+        -- elseif os.date("%Y", time) == os.date("%Y") then
+    else
+        time = os.date("%d/%m/%Y %H:%M", time)
+    end
+
+    local size = self._file:size()
+    return string.format("%s  %s", size and ya.readable_size(size) or "-", time)
+end
