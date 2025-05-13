07fc480e59445c0933231fc3c08b2040fa1ac4c1
Michael Baklor <mbaklor@gmail.com>
2025-05-13 22:02:57 +0300

    chore(yazi): just some type stuff in init.lua

--- yazi/init.lua
+++ yazi/init.lua
@@ -2,13 +2,14 @@ require("git"):setup()
 
 function Linemode:size_and_mtime()
 	local time = math.floor(self._file.cha.mtime or 0)
-	if time == 0 then
-		time = ""
+	local timefmt = ""
+	if time ~= 0 then
+		-- timefmt = ""
 		-- elseif os.date("%Y", time) == os.date("%Y") then
-	else
-		time = os.date("%d/%m/%Y %H:%M", time)
+		-- else
+		timefmt = os.date("%d/%m/%Y %H:%M", time)
 	end
 
 	local size = self._file:size()
-	return string.format("%s  %s", size and ya.readable_size(size) or "-", time)
+	return string.format("%s  %s", size and ya.readable_size(size) or "-", timefmt)
 end
