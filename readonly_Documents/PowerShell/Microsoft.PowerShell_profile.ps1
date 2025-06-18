834337a485c1ebf5bf68b9881d18ea6c79a6da81
Michael B <mbaklor@gmail.com>
2025-06-18 15:15:46 +0300

    feat(pwsh): turns out I'm using emacs bindings

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -16,11 +16,12 @@ function prompt
     return $out
 }
 
+Set-PSReadLineOption -EditMode Emacs
+
 # ReadLine handlers
 Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
 
-Set-PSReadLineKeyHandler -Chord Ctrl+y -Function AcceptSuggestion
 Set-PSReadLineKeyHandler -Chord Ctrl+p -ScriptBlock {
     $cons=[Microsoft.PowerShell.PSConsoleReadLine]
     $cons::SetMark()
@@ -33,21 +34,6 @@ Set-PSReadLineKeyHandler -Chord Ctrl+n -ScriptBlock {
     $cons::HistorySearchForward()
     $cons::EndOfLine()
 }
-Set-PSReadLineKeyHandler -Chord Alt+x -Function ExchangePointAndMark
-
-Set-PSReadLineKeyHandler -Chord Ctrl+a -Function BeginningOfLine
-Set-PSReadLineKeyHandler -Chord Ctrl+e -Function EndOfLine
-Set-PSReadLineKeyHandler -Chord Ctrl+b -Function BackwardChar
-Set-PSReadLineKeyHandler -Chord Alt+b -Function BackwardWord
-Set-PSReadLineKeyHandler -Chord Ctrl+f -Function ForwardChar
-Set-PSReadLineKeyHandler -Chord Alt+f -Function ForwardWord
-Set-PSReadLineKeyHandler -Chord Ctrl+u -Function BackwardDeleteLine
-
-# Set-PSReadLineKeyHandler -Chord Ctrl+f -ScriptBlock {
-#     [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-#     [Microsoft.PowerShell.PSConsoleReadLine]::Insert("ffd")
-#     [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-# }
 
 function conf([Parameter(Mandatory=$true)][string]$config)
 {
